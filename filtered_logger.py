02a96107d642d542df1104d9ba9029dbd85c617f
reinhard <reinhard2057@gmail.com>
2023-05-26 22:31:46 +0000

    task 0

---
+++ filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+contains a function called filter_datum that
+returns the log message obfuscated
+"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """redact document like i'm in the FBI ;)"""
+    for field in fields:
+        message = re.sub(r'{}=(\w+|\d+\/\d+\/\d+);'.format(field),
+                         '{}={};'.format(field, redaction), message)
+    return message
