e28cae934b759fa9b720f15a197e4a92f2c20a49
reinhard <reinhard2057@gmail.com>
2023-06-01 14:55:10 +0000

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+implements the sessionAuth class that
+handles session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """session auth class"""
+    pass
