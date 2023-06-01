6c396a2a84a1f0db3cfb3af7577de0270ba35d03
reinhard <reinhard2057@gmail.com>
2023-06-01 15:53:29 +0000

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ handles session authentication
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -28,3 +29,10 @@ class SessionAuth(Auth):
             return None
 
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns the user based"""
+        sess_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(sess_id)
+        user = User.get(user_id)
+        return user
