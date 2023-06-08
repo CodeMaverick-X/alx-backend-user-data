4db9c3b06d3c223ed08af0e03c3987f438af4644
reinhard <reinhard2057@gmail.com>
2023-06-08 19:32:21 +0000

    task_18: implement update password method for auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,3 +93,14 @@ class Auth:
             reset_t = str(uuid.uuid4())
             self._db.update_user(user.id, reset_token=reset_t)
             return reset_t
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates user password for valid reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except (NoResultFound):
+            raise ValueError
+        else:
+            hashed_p = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_p)
+            self._db.update_user(user.id, reset_token=None)
