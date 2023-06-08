4ec557af490189b4df8ce68ca87182de9d8b5279
reinhard <reinhard2057@gmail.com>
2023-06-08 19:41:06 +0000

    task_19: implement route for update password in class

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,5 +80,20 @@ def reset_password():
         return jsonify({"email": email, "reset_token": token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """endpoint for update password"""
+    email = request.form.get('email')
+    reset_t = request.form.get('reset_token')
+    new_pass = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_t, new_pass)
+    except (ValueError):
+        abort(403)
+    else:
+        return jsonify({"email": "<user email>",
+                        "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
